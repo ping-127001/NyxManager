2fdb6f853f4b7a2e8936e9704f794ab42b6f6039
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-05-15 00:04:23 -0400

    Add files via upload

---
+++ NyxManager/GUI/MainFrm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NyxManager.Handler;
+
+namespace NyxManager.GUI
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void miniBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageWndw(ex.Message);
+            }
+        }
+
+        void MessageWndw(string msg)
+        {
+            Data.msg = msg;
+            MessageCatcher msgcatcher = new MessageCatcher();
+            msgcatcher.ShowDialog();
+        }
+    }
+}
