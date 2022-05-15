188e20f3f949734554b279e22b9f956b12067c38
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-05-14 23:45:12 -0400

    Add files via upload

---
+++ NyxManager/Handler/Net.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NyxManager.Handler
+{
+    internal class Net
+    {
+        public static string webstring;
+        public static bool WebStringTrue;
+
+        public static void ReadWebString(string website)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                webstring = client.DownloadString(website);
+                //if you want to print out the string you can ig
+                //Messagebox.Show(webstring);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName);
+            }
+        }
+
+        public static void CompareWebString(string website, string compare)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                if (client.DownloadString(website).Contains(compare))
+                {
+                    //the user could now do a check like
+                    /*
+                     * if (WebStringTrue)
+                     * {
+                     *   code here
+                     *  }
+                     *  else
+                     *  {
+                     *  
+                     *  }
+                    */
+                    WebStringTrue = true;
+                }
+                else
+                {
+                    WebStringTrue = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName);
+            }
+        }
+
+        public static void DownloadFile(string url, string fileName)
+        {
+            WebClient client = new WebClient();
+            WebClient myWebClient = new WebClient();
+            myWebClient.DownloadFile(url, fileName);
+        }
+    }
+}
